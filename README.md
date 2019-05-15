8bf8fb8a2c14d6e2e1a9ee42b503feeeb806cba3
Sundance <sundancekid1108@gmail.com>
2019-05-15 15:52:55 +0900

    graphql 설정 완료
    graphql 서버 설정, 쿼리 테스트

---
+++ README.md
@@ -0,0 +1,6 @@
+# graphqlstudy
+
+Apollo와 Graphql 연습
+
+#GraphQl로 해결할수 있는것
+- over-fetching, under-fetching
---
+++ movieql-server/graphql/resolvers.js
@@ -0,0 +1,9 @@
+//query에 대한 response
+
+const resolvers = {
+    Query: {
+        name: () => "cheol"
+    }
+}
+
+export default resolvers;
